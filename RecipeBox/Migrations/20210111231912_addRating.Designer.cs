4d5b94fe63c5e3341d5d2547a5c896d9b89a0052
Tawnee Harris <tawneeh@icloud.com>
2021-01-11 15:41:16 -0800

    add addRating migration
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- RecipeBox/Migrations/20210111231912_addRating.Designer.cs
+++ RecipeBox/Migrations/20210111231912_addRating.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecipeBox.Models;
 
 namespace RecipeBox.Migrations
 {
     [DbContext(typeof(RecipeBoxContext))]
-    partial class RecipeBoxContextModelSnapshot : ModelSnapshot
+    [Migration("20210111231912_addRating")]
+    partial class addRating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
