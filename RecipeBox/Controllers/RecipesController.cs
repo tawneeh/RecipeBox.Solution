8925bdd364ae652226d90608689e6dbbf0dd5b03
Tawnee Harris <tawneeh@icloud.com>
2021-01-11 15:43:05 -0800

    update RecipesController
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
-
-//new using directives
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
